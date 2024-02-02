46bb183f0c1d9924fbef2bfd245b808d1b59edcb
Anouchka Dierckx <152172101+adierckx22@users.noreply.github.com>
2024-02-02 11:41:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -208,8 +208,8 @@ const predict = () => {
       })
       .then(response => response.json())
       .then(data => {
-        document.getElementById('fare').classList.remove('d-none');
-        const fareResult = document.getElementById('predicted-fare');
+        document.getElementById('fare_amount').classList.remove('d-none');
+        const fareResult = document.getElementById('fare_amount');
         const fare = Math.round(data['fare_amount'] * 100) / 100
         fareResult.innerText = `$${fare}`;
       })
